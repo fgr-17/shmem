58b1dee31a2cc1dd6fffdb97a75cec2e79beea24
Federico Roux <federico.roux@globant.com>
2022-12-26 11:42:01 -0300

    basic working shmem files

--- test/main_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include <fakeit.hpp>
-
-TEST_CASE("dummy test", "[C++ Template]") {
-  int i = 0;
-
-  INFO("Basic info from test app: " << i);
-  REQUIRE(true);
-}
